c2af6f06846d496b68ab09a07f20086bb43346bf
Carpediemconsultoria123 <sucessodocliente@carpediemconsultoria.com>
2025-02-20 15:09:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const script_do_google = 'https://script.google.com/macros/s/AKfycbxz7SUjg8_WNj3izVvf9V6lvZrR6t7SwPGtBTkS7I6KUSGmZ2Tp97L75fH0iY9-N5d0ow/exec';
+const script_do_google = 'https://script.google.com/macros/s/AKfycbzpr2gZxR_tMf8b-PPT6pC8ytEwsQKQm_BsqJk-knIOMU1G6s7uy-hToIx1FPx0VazFFQ/exec';
 const dados_do_formulario = document.forms['formulario-atendimento'];
 
 dados_do_formulario.addEventListener('submit', function(e) {
     e.preventDefault();
 
+    let formData = {
+        status_atendimento: dados_do_formulario.status_atendimento.value,
+        data_solicitacao: dados_do_formulario.data_solicitacao.value,
+        empresa: dados_do_formulario.empresa.value,
+        gestor_atendido: dados_do_formulario.gestor_atendido.value,
+        nivel_atencao: dados_do_formulario.nivel_atencao.value
+    };
+
     fetch(script_do_google, { 
         method: 'POST',
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(new FormData(dados_do_formulario))
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData)
     })
     .then(response => response.json()) 
     .then(data => {
